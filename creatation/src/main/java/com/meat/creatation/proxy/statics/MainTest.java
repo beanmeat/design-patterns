0e78fdbdfb3b26822690238d942a2bc2dbdd2fb4
tianchenhui <3090018282@qq.com>
2025-03-20 23:59:16 +0800

    JDK动态代理（proxy design)

--- creatation/src/main/java/com/meat/creatation/proxy/statics/MainTest.java
+++ creatation/src/main/java/com/meat/creatation/proxy/statics/MainTest.java
@@ -1,11 +1,13 @@
 package com.meat.creatation.proxy.statics;
 
 /**
- * 核心：在不改变原来的接口的情况下扩展新功能，或者增强方法
+ * 代理的东西不一样，每一种不同的被代理类都要创建不同的静态代理类
  */
 public class MainTest {
     public static void main(String[] args) {
         TikTokProxy proxy = new TikTokProxy(new TcTikTok());
         proxy.tiktok();
+
+        // 静态代理就是装饰器，装饰器模式就是代理模式的一种
     }
 }
